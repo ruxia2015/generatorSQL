088fd68612c94f60d5caaa7c12ceace93fcf8932
ruxia2015 <xuruxia@xrx-PC>
2015-06-10 09:58:32 +0800

    优化

--- src/generatorSQLScript/mysql/MysqlMgr.java
+++ src/generatorSQLScript/mysql/MysqlMgr.java
@@ -7,10 +7,8 @@ import generatorSQLScript.util.MysqlUtil;
 
 import java.sql.Connection;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.StringTokenizer;
 
 public class MysqlMgr
 {
